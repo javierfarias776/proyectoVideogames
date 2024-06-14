d3fe3226e40431deed981c35f367e8ed0e71fbd1
javierfarias776 <edwincarrilo.776@gmail.com>
2024-06-14 14:07:09 -0300

    subida de imagenes

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-let productos = [
-{id:1,nombre:"got", precio:1000, },
-{id:2,nombre:"cod", precio:1200},
-{id:3,nombre:"gta", precio:1300},
-{id:4,nombre:"fifa", precio:1400},
-{id:5,nombre:"mk11", precio:1500},
-{id:6,nombre:"The Last Of Us", precio:1600},
-{id:7,nombre:"Uncharted", precio:1700},
-{id:8,nombre:"Until Dawn", precio:1800 }
+  let productos = [
+{id:1,nombre:"got", precio:1000, img:"img/gow.jpeg" },
+{id:2,nombre:"cod", precio:1200, img:"img/cod.jpg"},
+{id:3,nombre:"gta", precio:1300, img:"img/gta2.jpg"},
+{id:4,nombre:"fifa", precio:1400, img:"img/fifa.jpg"},
+{id:5,nombre:"mk11", precio:1500, img:"img/mk2.jpg"},
+{id:6,nombre:"The Last Of Us", precio:1600, img:"img/theLastOfUs.jpg"},
+{id:7,nombre:"Uncharted", precio:1700, img:"img/uncharted.jpeg"},
+{id:8,nombre:"Until Dawn", precio:1800, img:"img/untilDawn.jpg"}
 ];
 localStorage.setItem("productos",JSON.stringify(productos))
-
 
 
 let recuperado = JSON.parse(localStorage.getItem("productos"))
